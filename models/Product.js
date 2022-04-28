32ffdd9e7cc60086dbb9679d38d68211976f0d46
Igor Rodrigues <igorr2693@gmail.com>
2022-04-27 21:22:41 -0300

    Resgatando todos os dados do banco

--- models/Product.js
+++ models/Product.js
@@ -2,8 +2,9 @@ const conn = require('../connection/conn')
 
 class Product {
 
-    constructor(name, price, description) {
+    constructor(name, image, price, description) {
         this.name = name
+        this.image = image
         this.price = price
         this.description = description
     }
@@ -11,13 +12,24 @@ class Product {
     save() {
         const product = conn.db().collection('products').insertOne({
             name: this.name,
+            image: this.image,
             price: this.price,
             description: this.description
+            
         })
 
         return product
 
+    } 
+
+    static getProducts() {
+        const products = conn.db().collection('products').find().toArray()
+
+        return products
     }
+
+
+
 }
 
 module.exports = Product
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -8,4 +8,7 @@ router.get('/', ProductController.showProducts)
 router.get('/create', ProductController.createProduct)
 router.post('/create', ProductController.createProductPost)
 
+
+
+
 module.exports = router
